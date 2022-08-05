76af614a40ba3b2953f150e97f5ecf1eba2e642e
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-05 13:29:17 +0100

    Ability to update profile

--- Profile.cs
+++ Profile.cs
@@ -65,6 +65,51 @@ namespace OctoNet_GUI
 
         }
 
+        private void UpdateName()
+        {
+            var name = new UserUpdate
+            {
+                Name = tb_name_on_profile.Text
+            };
+            client.User.Update(name);
+        }
+
+        private void UpdateBio()
+        {
+            var bio = new UserUpdate
+            {
+                Bio = tb_bio.Text
+            };
+            client.User.Update(bio);
+        }
+
+        private void UpdateLocation()
+        {
+            var location = new UserUpdate
+            {
+                Location = tb_location.Text
+            };
+            client.User.Update(location);
+        }
+
+        private void UpdateWebsite()
+        {
+            var website = new UserUpdate
+            {
+                Blog = tb_website.Text
+            };
+            client.User.Update(website);
+        }
+
+        private void UpdateCompany()
+        {
+            var company = new UserUpdate
+            {
+                Company = tb_company.Text
+            };
+            client.User.Update(company);
+        }
+
         private void web_pfp_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
 
@@ -72,7 +117,33 @@ namespace OctoNet_GUI
 
         private void bttn_save_changes_Click(object sender, EventArgs e)
         {
+            if (tb_name_on_profile.Text != user.Name.ToString())
+            {
+                UpdateName();
+            }
+            if (tb_bio.Text != user.Bio.ToString())
+            {
+                UpdateBio();
+            }
+            if (tb_location.Text != user.Location.ToString())
+            {
+                UpdateLocation();
+            }
+            if (tb_website.Text != user.Blog.ToString())
+            {
+                UpdateWebsite();
+            }
+            if (tb_company.Text != user.Company.ToString())
+            {
+                UpdateCompany();
+            }
 
+            tb_name_on_profile.Text = user.Name.ToString();
+            tb_bio.Text = user.Bio.ToString();
+            web_pfp.Url = new Uri(user.AvatarUrl.ToString());
+            tb_location.Text = user.Location.ToString();
+            tb_website.Text = user.Blog.ToString();
+            tb_company.Text = user.Company.ToString();
         }
 
         private void tb_website_TextChanged(object sender, EventArgs e)
