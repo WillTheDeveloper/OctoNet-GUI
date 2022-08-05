ab1ac3f2376924ffb98c4c3d61c273381a2f78b2
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-05 10:43:53 +0100

    Made issues pretty epic

--- Issues.Designer.cs
+++ Issues.Designer.cs
@@ -33,6 +33,8 @@
             this.lbl_issue_body = new System.Windows.Forms.Label();
             this.bttn_assignees = new System.Windows.Forms.Button();
             this.bttn_labels = new System.Windows.Forms.Button();
+            this.bttn_comments = new System.Windows.Forms.Button();
+            this.lbl_loading_state = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lb_issues
@@ -65,6 +67,7 @@
             // 
             // bttn_assignees
             // 
+            this.bttn_assignees.Enabled = false;
             this.bttn_assignees.Location = new System.Drawing.Point(394, 376);
             this.bttn_assignees.Name = "bttn_assignees";
             this.bttn_assignees.Size = new System.Drawing.Size(75, 23);
@@ -82,17 +85,39 @@
             this.bttn_labels.Text = "Labels";
             this.bttn_labels.UseVisualStyleBackColor = true;
             // 
+            // bttn_comments
+            // 
+            this.bttn_comments.Enabled = false;
+            this.bttn_comments.Location = new System.Drawing.Point(557, 375);
+            this.bttn_comments.Name = "bttn_comments";
+            this.bttn_comments.Size = new System.Drawing.Size(75, 23);
+            this.bttn_comments.TabIndex = 5;
+            this.bttn_comments.Text = "Comments";
+            this.bttn_comments.UseVisualStyleBackColor = true;
+            // 
+            // lbl_loading_state
+            // 
+            this.lbl_loading_state.AutoSize = true;
+            this.lbl_loading_state.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_loading_state.Location = new System.Drawing.Point(12, 406);
+            this.lbl_loading_state.Name = "lbl_loading_state";
+            this.lbl_loading_state.Size = new System.Drawing.Size(270, 26);
+            this.lbl_loading_state.TabIndex = 6;
+            this.lbl_loading_state.Text = "LOADING, PLEASE WAIT";
+            this.lbl_loading_state.Visible = false;
+            // 
             // Issues
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(795, 441);
+            this.Controls.Add(this.lbl_loading_state);
+            this.Controls.Add(this.bttn_comments);
             this.Controls.Add(this.bttn_labels);
             this.Controls.Add(this.bttn_assignees);
             this.Controls.Add(this.lbl_issue_body);
             this.Controls.Add(this.lbl_issue_name);
             this.Controls.Add(this.lb_issues);
-            this.Enabled = false;
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Issues";
             this.Text = "Issues";
@@ -109,5 +134,7 @@
         private System.Windows.Forms.Label lbl_issue_body;
         private System.Windows.Forms.Button bttn_assignees;
         private System.Windows.Forms.Button bttn_labels;
+        private System.Windows.Forms.Button bttn_comments;
+        private System.Windows.Forms.Label lbl_loading_state;
     }
 }
--- Issues.cs
+++ Issues.cs
@@ -20,25 +20,35 @@ namespace OctoNet_GUI
         public Issues()
         {
             InitializeComponent();
-
         }
 
         private void Issues_Load(object sender, EventArgs e)
         {
+            lb_issues.Enabled = false;
+            lbl_loading_state.Visible = true;
             var ls = client.Issue.GetAllForCurrent().Result;
 
             foreach (var issue in ls)
             {
                 lb_issues.Items.Add(issue.Title);
             }
-
+            lb_issues.Enabled = true;
+            lbl_loading_state.Visible = false;
         }
 
         private void lb_issues_SelectedIndexChanged(object sender, EventArgs e)
         {
+            lbl_loading_state.Visible = true;
+            lb_issues.Enabled = false;
+            
             var selectedRepo = lb_issues.SelectedItem.ToString();
             var selectedIssue = client.Issue.GetAllForCurrent().Result.Where(x => x.Title == selectedRepo).First();
+            
             lbl_issue_name.Text = selectedIssue.Title;
+            lbl_issue_body.Text = selectedIssue.Body;
+            
+            lb_issues.Enabled = true;
+            lbl_loading_state.Visible = false;
         }
     }
 }
